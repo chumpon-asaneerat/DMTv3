d3054b24114c901e68de315be44a604c8f09f682
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-13 20:51:50 +0700

    Update 2020-07-13 #21 - DMTModelBase class ported.

---
+++ 02.Models/01.DMT.Models/ExtensionMethods/ModelExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DMT.Models;
+
+#endregion
+
+namespace DMT.Models.ExtensionMethods
+{
+
+}
